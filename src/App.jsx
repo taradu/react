9d2e0fea3971378fed2357d458ccb27e37c91e65
Tar Kleiner <dubiukt@gmail.com>
2022-11-18 18:59:18 +0200

    work with input

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import React from 'react';
 function App() {
   const [items, setItems] = React.useState([])
   const [cartItems, setCartItems] = React.useState([])
+  const [searchValue, setSearchValue] = React.useState('')
   const [openCart, setOpenCart] = React.useState(false);
 
   React.useEffect(() => {
@@ -22,22 +23,26 @@ function App() {
   const onAddToCart = (obj) => {
     setCartItems([...cartItems, obj]);
   }
-
+  const onChangeSearchInput = (event) => {
+    setSearchValue(event.target.value);
+  }
   return (
     <div className="wrapper">
         { openCart ? <Rightside items={cartItems} onCloseCart={() => setOpenCart(false)}/> : null}
         <Header onClickCart={() => setOpenCart(true)} />
         <div className="content">
           <div className="inputAll">
-            <h1 className="allsneakers">Все кроссовки</h1>
+            <h1 className="allsneakers">{searchValue ? `Поиск по запросу: '${searchValue}'` : 'Все кроссовки'}</h1>
             <div className="search-block">
               <img src="/img/search.svg" alt="search" />
-              <input placeholder="Поиск..." />
+              {searchValue && <img onClick={() => setSearchValue('')} className="clear" src="/img/cartremove.svg" alt="clear" />}
+              <input onChange={onChangeSearchInput} value={searchValue} placeholder="Поиск..." />
             </div>
           </div>
         <div className="sneakers">
-        {items.map((item) => 
-        <Card title={item.title} 
+        {items.map((item, title) => 
+        <Card key={title}
+              title={item.title} 
               price={item.price} 
               img={item.imageURL} 
               onPlus={(obj) => onAddToCart(obj)}
